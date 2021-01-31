7dfaedcd60f03db73081b8241931d3a5ca6e6b16
Rocco <roccomaniscalco2001@gmail.com>
2021-01-31 03:36:41 -0500

    implemented additional config for deploying to heroku

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("client/public"));
+app.use(express.static("client/build"));
 
 app.listen(PORT, () => {
   console.log("listening on http://localhost:" + PORT);
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/public/index.html"));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
