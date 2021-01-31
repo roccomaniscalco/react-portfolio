19bb1fa5b1ec270690cc40d7080d25954060490e
Rocco <roccomaniscalco2001@gmail.com>
2021-01-30 23:28:45 -0500

    refactored Nav to use state

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Nav = () => {
+  const [navBarIsOpen, setNavBarIsOpen] = useState(false);
+
   const handleNavToggle = () => {
-    const nav = document.querySelector("nav");
-    nav.classList.toggle("open");
+    setNavBarIsOpen(!navBarIsOpen);
   };
 
   return (
     <>
       <header>
-      {/* eslint-disable-next-line */}
+        {/* eslint-disable-next-line */}
         <a id="logo" href="#">
           <strong>ROCCO</strong>
           <strong>MANISCALCO</strong>
         </a>
-        <i id="hamburger" onClick={handleNavToggle}>
+        <i
+          id="hamburger"
+          className={navBarIsOpen ? "open" : null}
+          onClick={handleNavToggle}
+        >
           <div id="line1"></div>
           <div id="line2"></div>
           <div id="line3"></div>
         </i>
       </header>
 
-      <nav>
+      <nav className={navBarIsOpen ? "open" : null}>
         <ul>
           <li>
             <a href="#about">About</a>
