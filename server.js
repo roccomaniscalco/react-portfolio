9e5e8b71b9341caa8bc0ee197d7bccd2fa631eff
Rocco <roccomaniscalco2001@gmail.com>
2021-01-31 03:27:12 -0500

    created server.js file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("client/public"));
+
+app.listen(PORT, () => {
+  console.log("listening on http://localhost:" + PORT);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/public/index.html"));
+});
